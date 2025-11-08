1659c1ec22f9ced94f6dc3f1285df1a1ef9add1c
MythSnipper <mythsnipper2022@gmail.com>
2025-11-08 21:57:51 +0200

    gdsadsgasadg

--- include/nucklib.hpp
+++ include/nucklib.hpp
@@ -10,6 +10,11 @@
 #include <fstream>
 
 #include <stb_image.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <imgui/imgui.h>
 #include <imgui/backends/imgui_impl_glfw.h>
 #include <imgui/backends/imgui_impl_opengl3.h>
